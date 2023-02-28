829f02b227e0e1991cb108c94a077a74a21a86f8
Daniel González Lopes <danielgonzalezlopes@gmail.com>
2023-02-28 11:37:10 +0100

    Add the metrics examples
    Signed-off-by: Daniel González Lopes <danielgonzalezlopes@gmail.com>

---
+++ 04-metrics.js
@@ -0,0 +1,32 @@
+import http from "k6/http";
+import { check, sleep } from "k6";
+import { Trend, Counter } from "k6/metrics";
+
+export const options = {
+  stages: [
+    { duration: '5s', target: 5 },
+    { duration: '10s', target: 5 },
+    { duration: '5s', target: 0 },
+  ],
+};
+
+const IMPORTANT = "everyone";
+
+const myTrend = new Trend('demo_waiting_time');
+const myCounter = new Counter('demo_taxis_dispatched')
+
+export function setup (){
+  console.log(`hello ${IMPORTANT}`)
+}
+
+export default function () {
+  let res = http.get(`http://localhost:8080/dispatch?customer=123`);
+  check(res, {"status is 200": (res) => res.status === 200});
+  myTrend.add(res.timings.waiting);
+  myCounter.add(1);
+  sleep(1);
+}
+
+export function teardown(){
+  console.log("bye")
+}
